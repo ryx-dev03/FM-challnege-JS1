3d075a5325e04973bec3083151f8ebab54c2533f
ryx-dev03 <triciodev@gmail.com>
2026-04-22 11:11:45 +0000

    final commit

--- JS/script.js
+++ JS/script.js
@@ -3,17 +3,19 @@
 const shareArrow = document.querySelector(".share-icon");
 const returnArrow = document.querySelector(".arrow.share-icon-1");
 const shareTab = document.querySelector(".share");
-const ParentTab = document.querySelector(".profile-content");
+const ParentTab = document.querySelector(".profile-wrapper");
+
 
 shareArrow.addEventListener("click", function (e) {
   e.stopPropagation();
   shareTab.classList.toggle("active");
-  
 
-  // ParentTab.classList.toggle("share-open");
+  ParentTab.classList.toggle("share-align");
 });
+
 
 returnArrow.addEventListener("click", function (e) {
   e.stopPropagation();
   shareTab.classList.toggle("active");
+  ParentTab.classList.toggle("share-align");
 });
